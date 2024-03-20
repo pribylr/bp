d0686e67baeb1225a37e2199f2cc7700a64e0d36
Radek Pribyl <pribyrad@fit.cvut.cz>
2024-03-20 18:42:06 +0100

    add autoformer source file

---
+++ src/autoformer.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import numpy as np
+
+class Autoformer(tf.keras.models.Model):
+    def __init__(self, **kwargs):
+        super(TFT, self).__init__()
+
+    def call(self, input):
+        # ...
+        return input
