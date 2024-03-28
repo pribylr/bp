b2d6f91960a17ce18d7a54e5d5ccb7559ac21960
Radek Pribyl <pribyrad@fit.cvut.cz>
2024-03-28 18:45:07 +0100

    autoformer: add data shapes clarification

--- src/autoformer.py
+++ src/autoformer.py
@@ -10,56 +10,56 @@ class Series_decomp(tf.keras.layers.Layer):  # ?
     def call(self, input):  # (batch, seq_len/2, features)
         X_t = self.avg_pool1d(input)  # (batch, seq_len/2, features)
         X_s = input - X_t  # (batch, seq_len/2, features)
-        return X_t, X_s
+        return X_s, X_t
 
 class Autocorrelation(tf.keras.layers.Layer):
     def __init__(self, config, **kwargs):
         super(Autocorrelation, self).__init__()
         self.d_k = config['d_k']
         self.d_v = config['d_v']
+        self.d_model = config['d_model']
         self.heads = config['ac_heads']
         self.c = config['c']
         self.input_seq_len = config['input_seq_len']
         
         self.query = tf.keras.layers.Dense(
-            config['d_k'], 
+            config['d_model']/config['ac_heads'], 
             kernel_initializer='glorot_uniform', 
             bias_initializer='glorot_uniform')
         self.key = tf.keras.layers.Dense(
-            config['d_k'],
+            config['d_model']/config['ac_heads'],
             kernel_initializer='glorot_uniform',
             bias_initializer='glorot_uniform')
         self.value = tf.keras.layers.Dense(
-            config['d_v'],
+            config['d_model']/config['ac_heads'],
             kernel_initializer='glorot_uniform',
             bias_initializer='glorot_uniform')
         self.out = tf.keras.layers.Dense(
-            config['d_k'],
+            config['d_model'],
             kernel_initializer='glorot_uniform',
             bias_initializer='glorot_uniform')
 
     def createQKV(self, input):
-        queries = [self.query(input[0]) for _ in range(self.heads)]  # [(batch, seq_len, d_k), ...]
-        keys = [self.query(input[1]) for _ in range(self.heads)]  # [(batch, seq_len, d_k), ...]
-        values = [self.query(input[2]) for _ in range(self.heads)]  # [(batch, seq_len, d_v), ...]
-        Q_4d = tf.stack(queries, axis=-1)  # (batch, seq_len, d_k, heads)
-        K_4d = tf.stack(keys, axis=-1)  # (batch, seq_len, d_k, heads)
-        V_4d = tf.stack(values, axis=-1)  # (batch, seq_len, d_v, heads)
+        queries = [self.query(input[0]) for _ in range(self.heads)]  # [(batch, seq_len, d_model/heads), ...]
+        keys = [self.query(input[1]) for _ in range(self.heads)]  # [(batch, seq_len, d_model/heads), ...]
+        values = [self.query(input[2]) for _ in range(self.heads)]  # [(batch, seq_len, d_model/heads), ...]
+        Q_4d = tf.stack(queries, axis=-1)  # (batch, seq_len, d_model/heads, heads)
+        K_4d = tf.stack(keys, axis=-1)  # (batch, seq_len, d_model/heads, heads)
+        V_4d = tf.stack(values, axis=-1)  # (batch, seq_len, d_model/heads, heads)
         Q_4d = tf.cast(Q_4d, tf.complex64)
         K_4d = tf.cast(K_4d, tf.complex64)
-        #V_4d = tf.cast(V_4d, tf.complex64)
-        Q_4d = tf.transpose(Q_4d, perm=[0, 3, 2, 1])  # (batch, heads, d_k, seq_len)
-        K_4d = tf.transpose(K_4d, perm=[0, 3, 2, 1])  # (batch, heads, d_k, seq_len)
-        V_4d = tf.transpose(V_4d, perm=[0, 3, 2, 1])  # (batch, heads, d_v, seq_len)
+        Q_4d = tf.transpose(Q_4d, perm=[0, 3, 2, 1])  # (batch, heads, d_model/heads, seq_len)
+        K_4d = tf.transpose(K_4d, perm=[0, 3, 2, 1])  # (batch, heads, d_model/heads, seq_len)
+        V_4d = tf.transpose(V_4d, perm=[0, 3, 2, 1])  # (batch, heads, d_model/heads, seq_len)
         return Q_4d, K_4d, V_4d
     
-    def time_delay_agg(self, input):  # (batch, heads, d_k, seq_len) (batch, heads, d_v, seq_len)
+    def time_delay_agg(self, input):  # (batch, heads, d_model/heads, seq_len) (batch, heads, d_model/heads, seq_len)
         qk_abs = tf.abs(input[0])
         k = int(self.c*np.log(input[0].shape[-1]))
-        values, indices = tf.math.top_k(qk_abs, k=k)  # (batch, heads, d_k, k)
+        values, indices = tf.math.top_k(qk_abs, k=k)  # (batch, heads, d_model/heads, k)
         values_sm = tf.nn.softmax(values)
-        def roll_each_feature(value_matrix, lag):  # (heads, d_k, seq_len) (heads, d_k)
-            rolled = tf.TensorArray(dtype=value_matrix.dtype, size=self.heads*self.d_v)
+        def roll_each_feature(value_matrix, lag):  # (heads, d_model/heads, seq_len) (heads, d_model/heads)
+            rolled = tf.TensorArray(dtype=value_matrix.dtype, size=self.d_model)
             idx = 0
             for head in range(self.heads):
                 for feature in range(self.d_v):
@@ -68,35 +68,35 @@ class Autocorrelation(tf.keras.layers.Layer):
                     rolled_v_slice = tf.roll(v_slice, shift=current_lag, axis=0)  # (seq_len, )
                     rolled = rolled.write(idx, rolled_v_slice)
                     idx += 1
-            result = tf.reshape(rolled.stack(), (self.heads, self.d_v, self.input_seq_len))  # (heads, d_v, seq_len)
+            result = tf.reshape(rolled.stack(), (self.heads, int(self.d_model/self.heads), self.input_seq_len))  # (heads, d_model/heads, seq_len)
             return result
             
         time_delay_aggregated = []
         for i in range(k):
             # highest values indices for all batches, heads, features, for day k
-            current_lag = -indices[..., i]  # (batch, heads, d_k)
+            current_lag = -indices[..., i]  # (batch, heads, d_model/heads)
             rolled_v =tf.map_fn(
                 fn=lambda elems: roll_each_feature(elems[0], elems[1]),
                 elems=(input[1], current_lag),
                 dtype=input[1].dtype
-            )  # (batch, heads, d_v, seq_len)
-            weighted_rolled_V = rolled_v * tf.expand_dims(values_sm[..., i], axis=-1)  # (batch, heads, d_v, seq_len)
+            )  # (batch, heads, d_model/heads, seq_len)
+            weighted_rolled_V = rolled_v * tf.expand_dims(values_sm[..., i], axis=-1)  # (batch, heads, d_model/heads, seq_len)
             time_delay_aggregated.append(weighted_rolled_V)
             
-        stacked = tf.stack(time_delay_aggregated, axis=-1)  # (batch, heads, d_v, seq_len, k)
-        aggregated_representation = tf.reduce_sum(tf.stack(time_delay_aggregated, axis=-1), axis=-1)  # (batch, heads, d_v, seq_len)
-        reshaped = tf.reshape(aggregated_representation, (aggregated_representation.shape[0], aggregated_representation.shape[1]*aggregated_representation.shape[2], aggregated_representation.shape[3]))  # (batch, heads*d_v, seq_len)
-        res = tf.transpose(reshaped, perm=[0, 2, 1])  # (batch, seq_len, d_v*heads)
+        stacked = tf.stack(time_delay_aggregated, axis=-1)  # (batch, heads, d_model/heads, seq_len, k)
+        aggregated_representation = tf.reduce_sum(tf.stack(time_delay_aggregated, axis=-1), axis=-1)  # (batch, heads, d_model/heads, seq_len)
+        reshaped = tf.reshape(aggregated_representation, (aggregated_representation.shape[0], aggregated_representation.shape[1]*aggregated_representation.shape[2], aggregated_representation.shape[3]))  # (batch, heads*d_model/heads, seq_len)
+        res = tf.transpose(reshaped, perm=[0, 2, 1])  # (batch, seq_len, d_model)
         return res
         
     def call(self, input):
-        Q, K, V = self.createQKV(input)  # (batch, heads, d_k, seq_len)
-        Q = tf.signal.fft(Q)  # (batch, heads, features, seq_len)
-        K = tf.signal.fft(K)  # (batch, heads, features, seq_len)
+        Q, K, V = self.createQKV(input)  # (batch, heads, d_model/heads, seq_len)
+        Q = tf.signal.fft(Q)  # (batch, heads, d_model/heads, seq_len)
+        K = tf.signal.fft(K)  # (batch, heads, d_model/heads, seq_len)
         K = tf.math.conj(K)
-        QK = tf.multiply(Q, K)  # (batch, heads, d_k, seq_len)
+        QK = tf.multiply(Q, K)  # (batch, heads, d_model/heads, seq_len)
         QK = tf.signal.ifft(QK)
-        delay = self.time_delay_agg((QK, V))  # (batch, seq_len, d_v*heads)
+        delay = self.time_delay_agg((QK, V))  # (batch, seq_len, d_model)
         
         return delay
         
@@ -105,10 +105,14 @@ class EncoderLayer(tf.keras.layers.Layer):
     def __init__(self, config, **kwargs):
         super(EncoderLayer, self).__init__()
         self.autocorrelation = Autocorrelation(config)
-        
+        self.series_decomp = Series_decomp(config)
+        self.li
     
-    def call(self, input):
-        x = self.autocorrelation((input, input, input))
+    def call(self, input):  # (batch, seq_len, d_model)
+        x = self.autocorrelation((input, input, input))  # (batch, seq_len, d_model)
+        x += input
+        S, _ = self.series_decomp(x)
+        print('S', S.shape)
 
 
 class Encoder(tf.keras.layers.Layer):
@@ -118,13 +122,17 @@ class Encoder(tf.keras.layers.Layer):
         self.d_v = config['d_v']
         self.encoder_layers_num = config['encoder_layers']
         self.encoder_layers = [EncoderLayer(config) for _ in range(config['encoder_layers'])]
-        
+        self.embed = tf.keras.layers.Dense(
+            config['d_model'],
+            kernel_initializer='glorot_uniform',
+            bias_initializer='glorot_uniform')
 
     def call(self, input):
+        x = self.embed(input)  # (batch, seq_len, d_model)
         for i in range(self.encoder_layers_num):
-            input = self.encoder_layers[i](input)
+            x = self.encoder_layers[i](x)
         #ac = self.autocorrelation((input, input, input))
-        return input
+        return x
         
 class Decoder(tf.keras.layers.Layer):
     def __init__(self, config,**kwargs):
@@ -150,17 +158,17 @@ class Autoformer(tf.keras.models.Model):
         self.series_decomp = Series_decomp(config)
         
     def prepare_input(self, input):  # (batch, seq_len, features)
-        X_ent, X_ens = self.series_decomp(input[:, (input.shape[1])//2:, :])  # (batch, seq_len/2, features)
+        X_ens, X_ent = self.series_decomp(input[:, (input.shape[1])//2:, :])  # (batch, seq_len/2, features)
         mean = tf.reduce_mean(input, axis=1, keepdims=True)
         mean = tf.tile(mean, [1, self.O, 1])  # (batch, output_seq_len, features)
         zeros = tf.zeros([input.shape[0], self.O, input.shape[2]], dtype=tf.float32)  # (batch, output_seq_len, features)
         
         X_det = tf.concat([X_ent, mean], axis=1)  # (batch, seq_len/2 +output_seq_len, features)
         X_des = tf.concat([X_ens, zeros], axis=1)  # (batch, seq_len/2 +output_seq_len, features)
-        return X_det, X_des
+        return X_des, X_det
     
     def call(self, input):
-        X_det, X_des = self.prepare_input(input)
+        X_des, X_det = self.prepare_input(input)
         enc_out = self.encoder(input)
         dec_out = self.decoder((enc_out, X_det, X_des))
         return dec_out
