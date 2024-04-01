c62e6a309bba4bea1aad1ff1cdd25e2821603908
Radek Pribyl <pribyrad@fit.cvut.cz>
2024-04-01 09:58:42 +0200

    .

--- src/process_data.py
+++ src/process_data.py
@@ -9,6 +9,7 @@ def create_Xy_data(data_for_X, data_for_y, input_seq_len: int, output_seq_len: i
         ydata.append(data_for_y.iloc[i:i+output_seq_len])
     return np.array(Xdata), np.array(ydata)
 
+
 def split_data(Xdata, ydata, train_pct, val_pct):
     train_val_split = int(train_pct*(Xdata.shape[0]))
     val_test_split = int((train_pct+val_pct)*(Xdata.shape[0]))
@@ -16,11 +17,13 @@ def split_data(Xdata, ydata, train_pct, val_pct):
             Xdata[:train_val_split:val_test_split], ydata[:train_val_split:val_test_split], \
             Xdata[val_test_split:], ydata[val_test_split:]
 
+
 def diff_features(data: pd.DataFrame, period: int=1):
     for col in data.columns:
         data[col] = data[col].diff(period)
     data = data[1:]  # first row NaN
     return data
+
 
 def standardize_data(data: pd.DataFrame, train_pct: float):
     # use information only from training part of the dataset
