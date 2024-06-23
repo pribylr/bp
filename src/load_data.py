8aed8e52b27f542ded08ba4dc4d99f7b60788c95
Radek Pribyl <pribyrad@fit.cvut.cz>
2024-06-23 15:28:10 +0200

    add method for dataset load

--- src/load_data.py
+++ src/load_data.py
@@ -1,5 +1,18 @@
 import pandas as pd
 
 class DataLoader():
+    """
+    Class loads csv files with financial data
+    and executes basic transformation the data into pandas dataframe
+    """
     def __init__(self):
         pass
+
+    def load_eurusd_15min(self):
+        """
+        forex data eurusd --- one pip == 0.0001
+        """
+        data = pd.read_csv('data/15min1h4h/EURUSD15.csv', sep='\t')
+        data = data[(int(data.shape[0]/2)):]
+        data.columns = ['time', 'open', 'high', 'low', 'close', 'volume']
+        return data, 10000
