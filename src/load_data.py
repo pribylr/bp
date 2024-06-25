302fef48e664fb580c6b2f5d0d427766591aa213
Radek Pribyl <pribyrad@fit.cvut.cz>
2024-06-25 22:28:30 +0200

    .

--- src/load_data.py
+++ src/load_data.py
@@ -10,9 +10,9 @@ class DataLoader():
 
     def load_eurusd_15min(self):
         """
-        forex data eurusd --- one pip == 0.0001
+        forex data eurusd --- one pip == 0.0001 usd
         """
         data = pd.read_csv('data/15min1h4h/EURUSD15.csv', sep='\t')
-        data = data[(int(data.shape[0]/2)):]
+        #data = data[(int(data.shape[0]/2)):]
         data.columns = ['time', 'open', 'high', 'low', 'close', 'volume']
         return data, 10000
